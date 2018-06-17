f478133f404471aa2e369476b39c0f05fa8105da
Maxim Gekk <maxim.gekk@databricks.com>
2018-06-17 08:08:29 +0200

    Test for size of null map

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -571,10 +571,13 @@ class DataFrameFunctionsSuite extends QueryTest with SharedSQLContext {
       df.select(size($"a")),
       Seq(Row(2), Row(0), Row(3), Row(-1))
     )
+    checkAnswer(df.select(size($"a", 0)), Seq(Row(2), Row(0), Row(3), Row(0)))
     checkAnswer(
       df.selectExpr("size(a)"),
       Seq(Row(2), Row(0), Row(3), Row(-1))
     )
+    checkAnswer(df.selectExpr("size(a, -1)"),
+      Seq(Row(2), Row(0), Row(3), Row(-1)))
   }
 
   test("map_keys/map_values function") {
